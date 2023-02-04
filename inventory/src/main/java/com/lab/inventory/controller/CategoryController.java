da7c0ecd603866980722f615260a1194d2e5e767
rcruzrijo <rcruzrijo@gmail.com>
2023-02-04 19:04:36 -0400

    Project with CategoryController

---
+++ inventory/src/main/java/com/lab/inventory/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.lab.inventory.controller;
+
+import com.lab.inventory.controller.v1.model.CategoryResponseDTO;
+import com.lab.inventory.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inventory/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity <CategoryResponseDTO> getAllCategories() {
+        return new ResponseEntity<>(CategoryResponseDTO.builder()
+                .type("OK")
+                .message("Success")
+                .value(categoryService.getAll()).build(), HttpStatus.OK);
+    }
+
+}
